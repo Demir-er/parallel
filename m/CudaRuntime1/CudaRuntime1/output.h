ad7c67695197a28c1a7e110ee70f096f86b52835
Demir-er <98809871+Demir-er@users.noreply.github.com>
2026-05-10 10:41:15 +0300

    unused files are deleted

--- m/CudaRuntime1/CudaRuntime1/output.h
+++
@@ -1,8 +0,0 @@
-#ifndef OUTPUT_H
-#define OUTPUT_H
-
-#include <string>
-
-void outputResults(float cpuVaR, float gpuVaR, double cpuTime, double gpuTime);
-
-#endif 
--- m/CudaRuntime1/CudaRuntime1/simulate_cpu.h
+++
@@ -1,6 +0,0 @@
-// File: simulate_cpu.h
-#pragma once
-#include <vector>
-
-// CPU version calculation
-std::vector<float> simulateReturnsCPU(int numPaths, float meanReturn, float stdDev);
